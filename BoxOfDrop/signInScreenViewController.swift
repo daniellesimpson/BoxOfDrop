bb6d74ca23bb3b1e4852c1a061089efb4421d1d6
Danielle Simpson <danielle.simpson2@capitalone.com>
2016-08-22 09:40:00 -0400

    fixed error on popViewController

--- BoxOfDrop/signInScreenViewController.swift
+++ BoxOfDrop/signInScreenViewController.swift
@@ -17,11 +17,11 @@ class signInScreenViewController: UIViewController {
     }
 
     
+
+    
     @IBAction func goBack(sender: UIButton) {
         navigationController!.popViewControllerAnimated(true)
-
     }
-    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
